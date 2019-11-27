2073be8fa26f47fbf6cc0ae405f211c96cf716c7
mohamed gassama <bouyagassama@gmail.com>
2019-11-27 14:23:28 -0500

    frontend almost done

--- public/js/script.js
+++ public/js/script.js
@@ -2,7 +2,7 @@
   window.onload = () => {
     console.log('Js up and running!');
 
-    const getTodo = async () => {
+    const getTodos = async () => {
       try {
         const res = await fetch('http://localhost:9000/api/todos');
         const json = await res.json();
@@ -13,17 +13,78 @@
           const mainUl = document.createElement('ul');
           const subject = document.createElement('li');
           const content = document.createElement('li');
+          const btn = document.createElement('button');
           const hbr = document.createElement('hr');
           subject.innerHTML = json.data[i].subject;
           content.innerHTML = json.data[i].content;
+          btn.innerHTML = 'Show details';
           mainUl.append(subject);
           mainUl.append(content);
           mainDiv.append(mainUl);
+          mainDiv.append(btn);
+          mainDiv.append(hbr);
         }
       } catch (err) {
         console.error(err);
       }
     };
+
+    const getTodo = async id => {
+      try {
+        const res = await fetch(`http://localhost:9000/api/todos/${id}`);
+        const json = await res.json();
+        console.log(json);
+      } catch (err) {
+        console.error(err);
+      }
+    };
+
+    const createTodo = async () => {
+      try {
+        const res = await fetch('http://localhost:9000/api/todos', {
+          method: 'post',
+          headers: { 'Content-Type': 'application/json' },
+          body: JSON.stringify({})
+        });
+        const json = await res.json();
+        console.log(json);
+      } catch (err) {
+        console.error(err);
+      }
+    };
+
+    const updataTodo = async id => {
+      try {
+        const res = await fetch(`http://localhost:9000/api/todos${id}`, {
+          method: 'update',
+          headers: { 'Content-Type': 'application/json' },
+          body: JSON.stringify({})
+        });
+        const json = await res.json();
+        console.log(json);
+      } catch (err) {
+        console.error(err);
+      }
+    };
+
+    const deleteTodo = async id => {
+      try {
+        const res = await fetch(`http://localhost:9000/api/todos${id}`, {
+          method: 'delete',
+          headers: { 'Content-Type': 'application/json' },
+          body: JSON.stringify({})
+        });
+        const json = await res.json();
+        console.log(json);
+      } catch (err) {
+        console.error(err);
+      }
+    };
+
+    getTodos();
     getTodo();
+    createTodo();
+    updataTodo();
+    deleteTodo(12);
   };
 })();
